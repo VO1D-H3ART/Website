fe426f6ba7c8861892086da68b4a24a3c790d7e5
VO1D-H3ART <ali.ahmedeyork@gmail.com>
2025-01-13 22:41:46 -0500

    Update and rename main.js to buttons.js

--- buttons.js
+++ buttons.js
@@ -1,4 +1,6 @@
 const darkModeButton = document.getElementById('DMB');
+const NewVoidButton = document.getElementById('NB');
+
 
 darkModeButton.addEventListener('click', () => {
 	document.body.classList.toggle('DarkMode');
@@ -7,10 +9,21 @@ darkModeButton.addEventListener('click', () => {
         darkModeButton.textContent = "You're good no need press this";
 		darkModeButton.style.backgroundColor = "white"; 
         darkModeButton.style.color = "black";
+		
+		//NewVoidButton.style.backgroundColor "green";
+		
     } else {
         darkModeButton.textContent = "Press For Dark Mode!!";
 		darkModeButton.style.backgroundColor = "black"; 
-        darkModeButton.style.color = "white"; //
+        darkModeButton.style.color = "white"; 
+		
+		//NewVoidButton.style.backgroundColor ="blue";
     }
+	
+	});
+
+NewVoidButton.addEventListener('click', () => {
+	document.body.classList.toggle('NewVoid');
+	
 	
 	});
