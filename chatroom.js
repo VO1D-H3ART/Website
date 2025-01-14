46f978454763ab112e6b27b967761a4c989b7a8e
VO1D-H3ART <ali.ahmedeyork@gmail.com>
2025-01-14 18:10:46 -0500

    Update chatroom.js

--- chatroom.js
+++ chatroom.js
@@ -3,6 +3,17 @@ const ChatInput = document.getElementById('ChatInput');
 const messageInput = document.getElementById('messageInput');
 const ChatContainer = document.getElementById('ChatContainer');
 
+const UserNameForm = document.getElementById('UserNameForm');
+
+let storedUserName = ''; //sets the default username to User
+
+
+UserNameForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    storedUserName = document.getElementById('usernameInput').value;
+    
+});
+
 
 //Event listener for the chat input - allows the user to submit a message
 ChatInput.addEventListener('submit', function(event) {
@@ -16,14 +27,13 @@ ChatInput.addEventListener('submit', function(event) {
 
     event.preventDefault(); //prevents the screen from refreshing
     const message = messageInput.value; //pulls the value from the input field
-
     if (!message) return; //if the message is empty, it will not submit
 
+    
     const messageElement = document.createElement('p'); //creates a new paragraph element
-    messageElement.textContent = `${timeStamp} ${message}`;//sets the text content of the paragraph element to the message
+    messageElement.innerHTML = `${timeStamp} <u><em><strong>${storedUserName}</em></strong></u> ${message}`;//sets the text content of the paragraph element to the message
     ChatContainer.appendChild(messageElement);//appends the message to the chat container
 
     messageInput.value = ''; //clears the input field
     messageInput.focus();//focuses on the input field (clicked in state)
-  
     });
